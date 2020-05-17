547f71d1d04b2dff7d443aca9da0373263144a7e
thekaamdhenu <65357274+thekaamdhenu@users.noreply.github.com>
2020-05-17 15:46:51 -0700

    Add files via upload

---
+++ The Kamdhenu/HTML/mapdata.js
@@ -0,0 +1,252 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   //General settings
+    width: "700", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    
+    //State defaults
+    state_description: "State description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "http://google.com",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "Location description",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_url: "",
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+    //Zoom settings
+    zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: "",
+    manual_zoom: "yes"
+  },
+  state_specific: {
+    "1": {
+      name: "Andaman and Nicobar",
+      description: "Known as KALA PAANI, during the time of British-dominant India"
+    },
+    "2": {
+      color: "#f96765",
+      name: "Andhra Pradesh",
+      description: "Rank 9th under Top-10 Milk Producing States of India."
+    },
+    "3": {
+      name: "Arunachal Pradesh",
+      description: " "
+    },
+    "4": {
+      name: "Assam",
+      description: " "
+    },
+    "5": {
+      color: "#bd0a08",
+      name: "Bihar",
+      description: "The 4th largest Milk Producing State of India. "
+    },
+    "6": {
+      name: "Chandigarh",
+      description: " "
+    },
+    "7": {
+      name: "Chhattisgarh",
+      description: " "
+    },
+    "8": {
+      name: "Dadra and Nagar Haveli",
+      description: " "
+    },
+    "9": {
+      name: "Daman and Diu",
+      description: " "
+    },
+    "10": {
+      name: "Delhi",
+      description: " "
+    },
+    "11": {
+      name: "Goa",
+      description: " "
+    },
+    "12": {
+      color: "#bd0a08",
+      name: "Gujarat",
+      description: "The 3rd largest Milk producing state in India. The Origination Place of White Revolution in India."
+    },
+    "13": {
+      color: "#8c0706",
+      name: "Haryana",
+      description: "The 2nd largest Milk producing state in India."
+    },
+    "14": {
+      name: "Himachal Pradesh",
+      description: " "
+    },
+    "16": {
+      name: "Jharkhand",
+      description: " "
+    },
+    "17": {
+      name: "Karnataka",
+      description: " "
+    },
+    "18": {
+      name: "Kerala",
+      description: " "
+    },
+    "19": {
+      name: "Lakshadweep",
+      description: " "
+    },
+    "20": {
+      color: "#f96765",
+      name: "Madhya Pradesh",
+      description: "Holds 10th position under Top-10 Milk Producing states in India."
+    },
+    "21": {
+      color: "#f63635",
+      name: "Maharashtra",
+      description: "The 7th state in Top 10 Milk producing States of India."
+    },
+    "22": {
+      name: "Manipur",
+      description: " "
+    },
+    "23": {
+      name: "Meghalaya",
+      description: " "
+    },
+    "24": {
+      name: "Mizoram",
+      description: " "
+    },
+    "25": {
+      name: "Nagaland",
+      description: " "
+    },
+    "26": {
+      name: "Orissa",
+      description: " "
+    },
+    "27": {
+      name: "Puducherry",
+      description: " "
+    },
+    "28": {
+      color: "#ee0c0a",
+      name: "Punjab",
+      description: "The Top Agricultural state, holds 6th rank in Top-10 Milk producing states of India"
+    },
+    "29": {
+      color: "#f63635",
+      name: "Rajasthan",
+      description: "The 8th state under Top-10 Milk Producing States of India."
+    },
+    "30": {
+      name: "Sikkim",
+      description: " "
+    },
+    "31": {
+      color: "#ee0c0a",
+      name: "Tamil Nadu",
+      description: "5th in ranking under Top-10 Milk Producing state."
+    },
+    "32": {
+      name: "Tripura",
+      description: " "
+    },
+    "33": {
+      color: "#8c0706",
+      name: "Uttar Pradesh",
+      description: "Largest Milk Producing state in India, Rank 1 under Top-10 Milk Producing states of India."
+    },
+    "34": {
+      name: "Uttaranchal",
+      description: " "
+    },
+    "35": {
+      name: "West Bengal",
+      description: " "
+    },
+    "36": {
+      name: "Jammu and Kashmir",
+      description: " "
+    },
+    "37": {
+      name: "Telangana",
+      description: " "
+    }
+  },
+  locations: {},
+  labels: {},
+  regions: {
+    "0": {
+      states: [],
+      name: "Milk Producing"
+    }
+  },
+  data: {
+    data: {
+      "2": "2000",
+      "5": "7000",
+      "12": "8000",
+      "13": "9000",
+      "20": "1000",
+      "21": "4000",
+      "28": "5000",
+      "29": "3000",
+      "31": "6000",
+      "33": "10000"
+    }
+  }
+};
